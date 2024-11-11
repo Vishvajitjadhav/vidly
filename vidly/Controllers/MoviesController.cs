5f848e256f370812053630cd05350651626cc86e
Vishvajit Jadhav <vishvajitjadhav21@gmail.com>
2024-11-11 10:12:36 +0530

    Add project files.

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movie = new Movie() { Id = 1, Name = "Natasamrat" };
+            return View(movie);
+        }
+    }
+}
